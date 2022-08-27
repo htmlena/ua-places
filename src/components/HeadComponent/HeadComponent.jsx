bc37596cbdc2a32639fe3763b643ee66f8d27a4b
Lenka Shynkarova <lllee@post.cz>
2022-08-27 09:14:08 +0200

    head component extracted

---
+++ src/components/HeadComponent/HeadComponent.jsx
@@ -0,0 +1,11 @@
+import Head from "next/head";
+
+const HeadComponent = () => {
+  <Head>
+    <title>Ukrajinská jídla v ČR</title>
+    <meta name="description" content="Ukrajinské restaurace v Česku" />
+    <link rel="icon" href="/favicon.ico" />
+  </Head>;
+};
+
+export default HeadComponent;

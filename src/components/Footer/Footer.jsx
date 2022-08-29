4cfd02f1b0f233390066faabb737dbb83d434f7e
Lenka Shynkarova <lllee@post.cz>
2022-08-29 09:00:57 +0200

    clean-up

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div>Footer</div>
+    </footer>
+  );
+};
+
+export default Footer;
--- src/components/HeadComponent/HeadComponent.jsx
+++ src/components/HeadComponent/HeadComponent.jsx
@@ -1,11 +1,13 @@
 import Head from "next/head";
 
 const HeadComponent = () => {
-  <Head>
-    <title>Ukrajinská jídla v ČR</title>
-    <meta name="description" content="Ukrajinské restaurace v Česku" />
-    <link rel="icon" href="/favicon.ico" />
-  </Head>;
+  return (
+    <Head>
+      <title>Ukrajinská jídla v ČR</title>
+      <meta name="description" content="Ukrajinské restaurace v Česku" />
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+  );
 };
 
 export default HeadComponent;
